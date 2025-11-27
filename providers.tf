5bf18c0ebfe0504a69761cfb296df07b0989c967
lkkaira <lalit.kaira@hcltech.com>
2025-11-27 15:55:55 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,4 +1,13 @@
 
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0" # or the version you need
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
   subscription_id = "679f3d56-bed2-429f-9e31-4d7bf67e14c7"
