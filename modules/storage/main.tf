1cfa2a7a7afa92b0dddcbef80ae281995df8472c
lkkaira <lalit.kaira@hcltech.com>
2025-11-27 15:14:11 +0530

    Update main.tf

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -6,13 +6,6 @@ resource "azurerm_storage_account" "storage" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
- public_network_access_enabled = false
- 
-  network_rules {
-    default_action = "Deny"
-    bypass         = ["AzureServices"]
-    ip_rules       = []
-  }
 
 variable "storage_name" {}
 variable "resource_group_name" {}
@@ -21,3 +14,10 @@ variable "location" {}
 output "storage_id" {
   value = azurerm_storage_account.storage.id
 }
+public_network_access_enabled = false
+ 
+  network_rules {
+    default_action = "Deny"
+    bypass         = ["AzureServices"]
+    ip_rules       = []
+  }
